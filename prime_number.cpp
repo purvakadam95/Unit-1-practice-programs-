c2a9c1c3e3b64a2d407584939380e771f47a7595
purvakadam95 <rzqr8rqvg4@privaterelay.appleid.com>
2026-02-01 15:04:47 +0530

    Create prime_number.cpp

---
+++ prime_number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, flag = 0;
+    cin >> n;
+
+    for(int i = 2; i <= n/2; i++) {
+        if(n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag == 0)
+        cout << "Prime Number";
+    else
+        cout << "Not Prime Number";
+
+    return 0;
+}
